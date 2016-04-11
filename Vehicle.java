8d8f3ed8a92351f21f45f1c6484cc58605e94dcf
JanKamburg <j.kamburg@t-online.de>
2016-04-11 15:28:23 -0400

    Update Vehicle.java

--- Vehicle.java
+++ Vehicle.java
@@ -1,10 +1,17 @@
-public class Vehicle { 
-    public enum MoveType{DRIVING, FLYING}; 
-    private MoveType moveType; 
-    private int maxSpeed; 
- 
-    public Vehicle(MoveType T , int MSpeed){ 
-        this.moveType = T; 
-        this.maxSpeed = MSpeed; 
-    } 
+public class Vehicle {
+    public enum MoveType{DRIVING, FLYING};
+    private MoveType moveType;
+    private int maxSpeed;
+
+    public Vehicle(MoveType T , int MSpeed){
+        this.moveType = T;
+        this.maxSpeed = MSpeed;
+    }
+
+    public int getMaxSpeed(){
+        return this.maxSpeed;
+    }
+    public MoveType getMoveType(){
+        return this.moveType;
+    }
 }
