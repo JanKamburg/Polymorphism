57855e163201e876c078de808fe2efdbaf7d032a
JanKamburg <j.kamburg@t-online.de>
2016-04-11 15:28:51 -0400

    Added plane

---
+++ Plane.java
@@ -0,0 +1,22 @@
+/**
+ * Created by Jan on 11.04.2016.
+ */
+public class Plane extends Vehicle {
+
+    private String Manufacturer;
+    private int maxPersonLoad;
+
+    public Plane(MoveType T, int MSpeed, String M, int P){
+        super(T,MSpeed);
+        this.Manufacturer = M;
+        this.maxPersonLoad = P;
+    }
+
+    public String getManufacturer(){
+        return this.Manufacturer;
+    }
+
+    public int getMaxPersonLoad(){
+        return this.maxPersonLoad;
+    }
+}
