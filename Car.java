4cd9fe858861fc2fb812c72ff3b161eb27ae3617
NeelRa <neel.ramani99@gmail.com>
2016-04-11 15:24:28 -0400

    Second change

--- Car.java
+++ Car.java
@@ -1,6 +1,13 @@
 public class Car extends Vehicle
 {
-  
-  
+  private String Manufacturer;
+  private String Model;
+    
+  public Car(MoveType M, int MS, String Manufacturer, String Model)
+  {
+    super(M,MS);
+    this.Manufacturer = Manufacturer;
+    this.Model = Model;
+  }
   
 }
